d9d791a4082c172e2ec365b592bb6af47f12b983
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-12-06 22:02:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/kindi.git /kindi
+  git clone https://github.com/Azanpopz/MY_JohnCena.git /MY_JohnCena
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /kindi
+  git clone $UPSTREAM_REPO /MY_JohnCena
 fi
 cd /kindi
 pip3 install -U -r requirements.txt
-echo "Starting kindi...."
+echo "Starting MY_JohnCena...."
 python3 bot.py
